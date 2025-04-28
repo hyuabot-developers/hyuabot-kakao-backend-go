e70942572b1edd51dadce11a2e552a177aa3a486
Jeongin Lee <jil8885@gmail.com>
2025-04-28 18:22:08 +0900

    feat: Implement response schema

--- schema/response.go
+++ schema/response.go
@@ -19,6 +19,47 @@ type (
 		Text      string    `json:"text"`
 	}
 
+	SimpleImage struct {
+		Component Component `json:"component,omitempty"`
+		ImageURL  string    `json:"imageUrl"`
+		AltText   string    `json:"altText"`
+	}
+
+	TextCard struct {
+		Component   Component    `json:"component,omitempty"`
+		Title       string       `json:"title"`
+		Description string       `json:"description"`
+		Buttons     []CardButton `json:"buttons"`
+	}
+
+	BasicCard struct {
+		Component   Component          `json:"component,omitempty"`
+		Title       string             `json:"title"`
+		Description string             `json:"description"`
+		Thumbnail   BasicCardThumbnail `json:"thumbnail"`
+		Buttons     []CardButton       `json:"buttons"`
+	}
+
+	BasicCardThumbnail struct {
+		ImageURL string `json:"imageUrl"`
+		AltText  string `json:"altText"`
+	}
+
+	CardButton struct {
+		Label       string `json:"label"`
+		Action      string `json:"action"`
+		WebLinkURL  string `json:"webLinkUrl"`
+		MessageText string `json:"messageText"`
+		PhoneNumber string `json:"phoneNumber"`
+		BlockID     string `json:"blockId"`
+	}
+
+	Carousel struct {
+		Component Component   `json:"component,omitempty"`
+		Type      string      `json:"type"`
+		Items     []Component `json:"items"`
+	}
+
 	QuickReply struct {
 		Label       string `json:"label"`
 		Action      string `json:"action"`
